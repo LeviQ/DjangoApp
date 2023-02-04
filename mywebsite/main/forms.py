665d43381c5097332f8dae7cba17b43e835d43f1
Aleksander Lewicki <aleksanderlewy218@gmail.com>
2023-02-04 18:43:49 +0100

    Added Items For section + little fixes

--- mywebsite/main/forms.py
+++ mywebsite/main/forms.py
@@ -13,7 +13,26 @@ class CreateNewList(forms.Form):
     )    
 
 class CreateNewItemForList(forms.Form):
-    name = forms.CharField(label="Name",max_length=200)
-    description = forms.CharField(label="Description",max_length=400,required=False)
+    name = forms.CharField(
+        label="Name",
+        max_length=200,
+        widget=forms.TextInput(attrs={
+            'style': 
+                'width: 20%; padding: 10px;'
+                'border: 1px solid #ddd; box-shadow: 2px 2px 5px #ddd; margin-right: 15px; margin-left: 5px;',
+            'placeholder': 'Item Name',
+        })
+    )
+    description = forms.CharField(
+        label="Description",
+        max_length=400,
+        required=False,
+        widget=forms.TextInput(attrs={
+            'style': 
+                'width: 30%; padding: 10px;'
+                'border: 1px solid #ddd; box-shadow: 2px 2px 5px #ddd; margin-right: 15px; margin-left: 5px;',
+            'placeholder': 'Item Description',
+        })
+    )
     completed = forms.BooleanField(label="Completed",required=False)
 
